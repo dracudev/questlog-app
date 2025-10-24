140dacf789421f4ed47588033ed162750e09aa2a
Javier Andreu <andreujavier99@gmail.com>
2025-10-24 11:54:24 +0200

    feat(games): add controllers and services for Developers, Publishers, Platforms, and Genres

--- apps/backend/src/games/games.module.ts
+++ apps/backend/src/games/games.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
+
+import { DevelopersController } from './developers/developers.controller';
+import { DevelopersService } from './developers/developers.service';
+import { PublishersController } from './publishers/publishers.controller';
+import { PublishersService } from './publishers/publishers.service';
+import { PlatformsController } from './platforms/platforms.controller';
+import { PlatformsService } from './platforms/platforms.service';
+import { GenresController } from './genres/genres.controller';
+import { GenresService } from './genres/genres.service';
+
 @Module({
-  controllers: [GamesController],
-  providers: [GamesService],
-  exports: [GamesService],
+  controllers: [
+    DevelopersController,
+    PublishersController,
+    PlatformsController,
+    GenresController,
+    GamesController,
+  ],
+  providers: [GamesService, DevelopersService, PublishersService, PlatformsService, GenresService],
+  exports: [GamesService, DevelopersService, PublishersService, PlatformsService, GenresService],
 })
 export class GamesModule {}
