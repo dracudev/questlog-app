95b5fb17296d2c1317dc6eb7cb418a3e83ee2e09
Javier Andreu <andreujavier99@gmail.com>
2025-11-03 22:07:56 +0100

    refactor(tailwind): remove unused Tailwind configuration file

--- apps/frontend/tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import baseConfig from '../../packages/config/src/tailwind/tailwind.config.js';
-
-export default {
-  ...baseConfig,
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    '../../packages/ui-components/src/**/*.{js,ts,jsx,tsx}',
-  ],
-};
